962a598e9f82978eba823fc5fafedd17dbb65dcc
Mátyás Angyal <amatyas001@gmail.com>
2020-02-22 15:20:49 +0100

    Updates

---
+++ precache-manifest.ffcd901379b1dee7e363929761c9751a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb5bcc4086c38d22ce4194ba90889f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2560c857c156f7a4ade1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2a06be65010a9460776d",
+    "url": "/static/js/2.f2d4f150.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f2d4f150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2560c857c156f7a4ade1",
+    "url": "/static/js/main.264076ad.chunk.js"
+  },
+  {
+    "revision": "1fb11721a4a82dceea11",
+    "url": "/static/js/runtime-main.8d189d82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
